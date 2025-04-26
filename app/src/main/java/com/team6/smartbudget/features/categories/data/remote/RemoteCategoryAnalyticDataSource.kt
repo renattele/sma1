05d419bdbfb3da5c668efe7ae11b012b39fca7c1
renattele <renattele@yandex.com>
2025-04-26 11:50:23 +0300

    Removed unnecessary features

--- app/src/main/java/com/team6/smartbudget/features/categories/data/remote/RemoteCategoryAnalyticDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.team6.smartbudget.features.categories.data.remote
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface RemoteCategoryAnalyticDataSource {
-    @GET("/analytic/categories")
-    suspend fun getAll(): Response<List<RemoteCategoryAnalyticDto>>
-}
